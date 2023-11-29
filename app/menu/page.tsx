55f0bd666318485f7aa05f1b85a10d229c85614b
Joshua Lehman <joshlehman.dev@gmail.com>
2023-11-29 16:30:15 -0500

    updates

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Menu.module.css";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <main className="main">
+      <section className={styles.menu_section}>
+        <h2>Menu</h2>
+        <div className={styles.spacer}></div>
+        <p className={styles.paragraph}>Drinks</p>
+        <div className={styles.spacer}></div>
+        <div className={styles.menu_divider} />
+      </section>
+    </main>
+  );
 };
 
 export default Menu;
